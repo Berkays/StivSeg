9df099e5811deb43777415c1f7b12ec73df7fa80
Berkay GURSOY <berkaygursoy@gmail.com>
2018-12-02 04:20:57 +0300

    add support for shift register

--- src/StivSeg.cpp
+++ src/StivSeg.cpp
@@ -7,140 +7,223 @@
  *RHDP = right hand decimal point
  */
 
-StivSeg::StivSeg(int numD, int dp[], int sp[], bool isCath) {
-    numDigits = numD;
+StivSeg::StivSeg(int dp[], int sp[], int latchPin, int clockPin, int dataPin, bool isCath, bool _useShiftRegister,int refreshRate)
+{
+    numDigits = sizeof(dp) / sizeof(int);
     isComCathode = isCath;
-    
+    useShiftRegister = _useShiftRegister;
+    hz = 1000 / refreshRate;
+
     //stores digit pins
-    for(int i = 0; i < numDigits; i++) {
+    for (int i = 0; i < numDigits; i++)
+    {
         digitPins[i] = dp[i];
         pinMode(digitPins[i], OUTPUT);
     }
-    
-    //stores segment pins
-    for(int i = 0; i <= 7; i++) {
-        segPins[i] = sp[i];
-        pinMode(segPins[i], OUTPUT);
+
+    if (useShiftRegister)
+    {
+        latch = latchPin;
+        clock = clockPin;
+        data = dataPin;
+
+        //set shift register pins
+        pinMode(latch, OUTPUT);
+        pinMode(clock, OUTPUT);
+        pinMode(data, OUTPUT);
     }
-    
+    else
+    {
+        //stores segment pins
+        for (int i = 0; i <= 7; i++)
+        {
+            segPins[i] = sp[i];
+            pinMode(segPins[i], OUTPUT);
+        }
+    }
+
     //determines common type and preps pin states
-    if(!isComCathode) {
-        for(int i = 0; i < numDigits; i++) {
+    if (!isComCathode)
+    {
+        for (int i = 0; i < numDigits; i++)
+        {
             digitalWrite(digitPins[i], HIGH);
         }
-        
-        for(int i = 0; i <= 7; i++) {
-            digitalWrite(segPins[i], HIGH);
+        if (useShiftRegister)
+        {
+            //Shift register is locked for high latch.
+            digitalWrite(latch, HIGH);
+        }
+        else
+        {
+            for (int i = 0; i <= 7; i++)
+            {
+                digitalWrite(segPins[i], HIGH);
+            }
         }
     }
 }
 
-StivSeg::writeInt(int n) {
-    int nray[numDigits];                
-    
+StivSeg::writeInt(int n)
+{
+    int nray[numDigits];
+
     //splits each digit of input and saves to a temporary array
-    for(int i = 0; i < numDigits; i++) {
+    for (int i = 0; i < numDigits; i++)
+    {
         nray[i] = n % 10;
         n /= 10;
     }
-    
+
     //writes to display here, decides how to treat display based off common type
-    if(isComCathode) {
+    if (isComCathode)
+    {
         //cycles through each dig
-        for(int dig = 0; dig < numDigits; dig++) {
+        for (int dig = 0; dig < numDigits; dig++)
+        {
             //cathode pulled low to allow seg illumination
             digitalWrite(digitPins[dig], LOW);
-            
-            //cycles through each seg and sets state in accordance with array SYMBOL[][]
-            for(int seg = 0; seg <= 6; seg++) {
-                digitalWrite(segPins[seg], SYMBOL[nray[dig]][seg]);
+
+            if (useShiftRegister)
+            {
+                digitalWrite(latch, LOW);
+                shiftOut(data, clock, MSBFIRST, nray[dig]);
+                digitalWrite(latch, HIGH);
             }
-            delay(10);
+            else
+            {
+                //cycles through each seg and sets state in accordance with array SYMBOL[][]
+                for (int seg = 0; seg <= 6; seg++)
+                {
+                    digitalWrite(segPins[seg], SYMBOL[nray[dig]][seg]);
+                }
+            }
+            delay(hz);
             //cathode pulled high to disallow illumination
             digitalWrite(digitPins[dig], HIGH);
         }
     }
-    else {
+    else
+    {
         //cycles through each dig in physical display
-        for(int dig = 0; dig < numDigits; dig++) {
+        for (int dig = 0; dig < numDigits; dig++)
+        {
             //anode pulled high to allow seg illumination
             digitalWrite(digitPins[dig], HIGH);
-            
-            //cycles through each seg and sets state in accordance with the boolean opposite of array SYMBOL[][]
-            for(int seg = 0; seg <= 6; seg++) {
-                digitalWrite(segPins[seg], !SYMBOL[nray[dig]][seg]);
+
+            if (useShiftRegister)
+            {
+                digitalWrite(latch, LOW);
+                shiftOut(data, clock, MSBFIRST, nray[dig]);
+                digitalWrite(latch, HIGH);
             }
-            delay(10);
+            else
+            {
+                //cycles through each seg and sets state in accordance with the boolean opposite of array SYMBOL[][]
+                for (int seg = 0; seg <= 6; seg++)
+                {
+                    digitalWrite(segPins[seg], !SYMBOL[nray[dig]][seg]);
+                }
+            }
+            delay(hz);
             //anode pulled low to disallow illumination
             digitalWrite(digitPins[dig], LOW);
         }
     }
 }
 
-StivSeg::writeFloat(float n) {
+StivSeg::writeFloat(float n)
+{
     int nray[numDigits];
     int pointPos = numDigits + 1;
-    
+
     //determines which physical display digit will "own" the decimal point (based off RHDP)
-    while(n > 1.0) {
+    while (n > 1.0)
+    {
         n /= 10.0;
         pointPos--;
     }
     //converts input to integer
     n *= (float)pow(10.0, (float)numDigits);
-    
-    for(int i = 0; i < numDigits; i++) {
+
+    for (int i = 0; i < numDigits; i++)
+    {
         nray[i] = (int)n % 10;
         n /= 10;
     }
-    
-    if(isComCathode) {
-        for(int dig = 0; dig < numDigits; dig++) {
+
+    if (isComCathode)
+    {
+        for (int dig = 0; dig < numDigits; dig++)
+        {
             digitalWrite(digitPins[dig], LOW);
-            
-            for(int seg = 0; seg <= 6; seg++) {
+
+            for (int seg = 0; seg <= 6; seg++)
+            {
                 digitalWrite(segPins[seg], SYMBOL[nray[dig]][seg]);
             }
-            
+
             //sets RHDP
-            if(pointPos == dig + 1) {
+            if (pointPos == dig + 1)
+            {
                 digitalWrite(segPins[7], HIGH);
             }
-            else {
+            else
+            {
                 digitalWrite(segPins[7], LOW);
             }
-            delay(10);
+            delay(hz);
             digitalWrite(digitPins[dig], HIGH);
         }
     }
-    else {
-        for(int dig = 0; dig < numDigits; dig++) {
+    else
+    {
+        for (int dig = 0; dig < numDigits; dig++)
+        {
             digitalWrite(digitPins[dig], HIGH);
-            
-            for(int seg = 0; seg <= 6; seg++) {
+
+            for (int seg = 0; seg <= 6; seg++)
+            {
                 digitalWrite(segPins[seg], !SYMBOL[nray[dig]][seg]);
             }
-            
+
             //sets RHDP
-            if(pointPos == dig + 1) {
+            if (pointPos == dig + 1)
+            {
                 digitalWrite(segPins[7], LOW);
             }
-            else {
+            else
+            {
                 digitalWrite(segPins[7], HIGH);
             }
-            delay(10);
+            delay(hz);
             digitalWrite(digitPins[dig], LOW);
         }
     }
 }
 
-StivSeg::clear() {
+StivSeg::clear()
+{
     //sets all dig pins and seg pins to low
-    for(int dig = 0; dig < numDigits; dig++) {
-        digitalWrite(digitPins[dig], LOW);
-        
-        for(int seg = 0; seg <= 7; seg++) {
-            digitalWrite(segPins[seg], LOW);
+    for (int dig = 0; dig < numDigits; dig++)
+    {
+        if (isComCathode)
+            digitalWrite(digitPins[dig], HIGH);
+        else
+            digitalWrite(digitPins[dig], LOW);
+
+        if (useShiftRegister)
+        {
+            digitalWrite(latch, LOW);
+            shiftOut(data, clock, MSBFIRST, digit_map[15]); //Set blank
+            digitalWrite(latch, HIGH);
+        }
+        else
+        {
+            for (int seg = 0; seg <= 7; seg++)
+            {
+                digitalWrite(segPins[seg], LOW);
+            }
         }
     }
 }
--- src/StivSeg.h
+++ src/StivSeg.h
@@ -3,30 +3,58 @@
 
 #include "Arduino.h"
 
-class StivSeg {
-    public:
-        StivSeg(int numD, int dp[], int sp[], bool isCath);
-        writeInt(int n);
-        writeFloat(float n);
-        clear();
-        
-    private:
-        int numDigits;
-        int digitPins[4];
-        int segPins[8];
-        bool isComCathode;
-        bool SYMBOL[11][7] = {
+class StivSeg
+{
+  public:
+    StivSeg(int dp[], int sp[], int latchPin, int clockPin, int dataPin, bool isCath, bool _useShiftRegister,int refreshRate);
+    writeInt(int n);
+    writeFloat(float n);
+    writeShiftInt(int n);
+    writeShiftFloat(float n);
+    clear();
+
+  private:
+    int numDigits;
+    int digitPins[4];
+    int segPins[8];
+    int latch;
+    int clock;
+    int data;
+    int hz;
+    bool isComCathode;
+    bool useShiftRegister;
+    const byte digit_map[16] =
+    {
+        B00111111, // 0
+        B00000110, // 1
+        B01011011, // 2
+        B01001111, // 3
+        B01100110, // 4
+        B01101101, // 5
+        B01111101, // 6
+        B00000111, // 7
+        B01111111, // 8
+        B01101111, // 9
+        B01110111, // A
+        B01111100, // b
+        B00111001, // C
+        B01011110, // d
+        B01111001, // E
+        B01110001, // F
+        B00000000  // BLANK
+    };
+    bool SYMBOL[11][7] = {
         //A     B      C     D     E     F     G
-        {true, true, true, true, true, true, false},    //0
-        {false, true, true, false, false, false, false},//1
-        {true, true, false, true, true, false, true},   //2
-        {true, true, true, true, false, false, true},   //3
-        {false, true, true, false, false, true, true},  //4
-        {true, false, true, true, false, true, true},   //5
-        {true, false, true, true, true, true, true},    //6
-        {true, true, true, false, false, false, false}, //7
-        {true, true, true, true, true, true, true},     //8
-        {true, true, true, false, false, true, true},   //9
+        {true, true, true, true, true, true, false},     //0
+        {false, true, true, false, false, false, false}, //1
+        {true, true, false, true, true, false, true},    //2
+        {true, true, true, true, false, false, true},    //3
+        {false, true, true, false, false, true, true},   //4
+        {true, false, true, true, false, true, true},    //5
+        {true, false, true, true, true, true, true},     //6
+        {true, true, true, false, false, false, false},  //7
+        {true, true, true, true, true, true, true},      //8
+        {true, true, true, false, false, true, true},    //9
         //{true, true, true, false, true, true, true},    //A
         //{false, false, true, true, true, true, true},   //b
         //{true, false, false, true, true, true, false},  //C
@@ -34,7 +62,7 @@ class StivSeg {
         //{true, false, false, true, true, true, true},   //E
         //{true, false, false, false, true, true, true},  //F
         {false, false, false, false, false, false, false} //blank
-        }; 
+    };
 };
 
 #endif
